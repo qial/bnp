41f27f5e40b077d0e2068b7c9e593cc33f74ff20
Kyle Watson <kw@qial.net>
2013-07-11 18:54:33 -0700

    Finished the config loading skeleton with a ton of TODOs in the confusing parts

--- bnp-osgi/src/main/java/com/headwire/bnp/NodeRunner.java
+++ bnp-osgi/src/main/java/com/headwire/bnp/NodeRunner.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.headwire.bnp.config.ActivityConfig;
+import com.headwire.bnp.config.ConditionConfig;
 import com.headwire.bnp.config.ProcessingConfiguration;
 
 public class NodeRunner {
@@ -17,29 +18,50 @@ public class NodeRunner {
 	}
 	
 	private void init(ProcessingConfiguration config) {
+		activities = new ArrayList<ProcessingActivity>();
+		
 		// TODO error checking for if ProcessingConfiguration isn't set up right
-		for(ActivityConfig activity : config.getActivities()) {
-			String className = activity.getClassName();
+		for(ActivityConfig aConfig : config.getActivities()) {
+			String activityClass = aConfig.getClassName();
 			
+			// get activity instance & load everything but conditions
+			ProcessingActivity activity = getProcessingActivityInstance(activityClass);
+			initProcessingActivity(activity,aConfig);
+
+			// go through conditions
+			List<ProcessingCondition> conditions = new ArrayList<ProcessingCondition>();
 			
+			// TODO can the aConfig.getConditions() be null?
+			for(ConditionConfig cConfig : aConfig.getConditions()) {
+				String conditionClass = cConfig.getClassName();
+				
+				// get condition instance & configure it
+				ProcessingCondition condition = getProcessingConditionInstance(conditionClass);
+				initProcessingCondition(condition, cConfig);
+				
+				// add condition to list for this activity
+				conditions.add(condition);
+			}
+			
+			// add conditions to activity and add activity to the list
+			activity.setConditions(conditions);
+			activities.add(activity);
 		}
 	}
 	
 	// pieces of info we need
 	private String startPath;
 	
-	// key is string to replace, value is replacement
-//	private Map<String,String> changes;
-	
-	// possible things we could need
-//	private String[] componentTypes;
-	
 	// the activities it'll be doing on nodes
 	private List<ProcessingActivity> activities = new ArrayList<ProcessingActivity>();
 	
-//	public void changeValuesOnNode() {
-//		
-//	}
+	private void initProcessingActivity(ProcessingActivity activity, ActivityConfig config) {
+		// TODO fill in variables
+	}
+	
+	private void initProcessingCondition(ProcessingCondition condition, ConditionConfig config) {
+		// TODO fill in variables
+	}
 	
 	private ProcessingActivity getProcessingActivityInstance(String className) {
 		Class activityClass = null;
