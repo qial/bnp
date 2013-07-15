6e0dc03dea00d9695bc7582702f01b772e4c1d62
Kyle Watson <kw@qial.net>
2013-07-15 15:19:20 -0700

    Added testrun component and made it run the test

--- bnp-content/src/main/content/jcr_root/apps/bnp/components/testrun/testrun.jsp
+++ bnp-content/src/main/content/jcr_root/apps/bnp/components/testrun/testrun.jsp
@@ -7,6 +7,9 @@
 --%><%
 %><%@include file="/libs/foundation/global.jsp"%><%
 %><%@page session="false" %><%
-%><%
+%><%@page import="com.cqblueprints.bnp.*" %><%
 	// TODO add you code here
-%>
+	
+	// lets run the test runner
+	TestJCR.runNodeRunner("test-config.json",resourceResolver);
+%>Test node ran?
--- bnp-osgi/src/main/java/com/headwire/bnp/TestJCR.java
+++ bnp-osgi/src/main/java/com/headwire/bnp/TestJCR.java
@@ -1,5 +1,28 @@
 package com.headwire.bnp;
 
-public class TestJCR {
+import javax.jcr.RepositoryException;
 
+import org.apache.sling.api.resource.ResourceResolver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.headwire.bnp.config.ProcessingConfiguration;
+import com.headwire.bnp.config.ProcessingConfigurationLoader;
+
+public class TestJCR {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(TestJCR.class);
+	
+	public static void runNodeRunner(String configFilePath, ResourceResolver rr) {
+		LOG.info("Running NodeRunner on config file "+configFilePath);
+		
+		ProcessingConfiguration config = ProcessingConfigurationLoader.loadConfiguration("test-config.json");
+		NodeRunner runner = new NodeRunner(config);
+		
+		try {
+			runner.run(rr);
+		} catch (RepositoryException e) {
+			LOG.error("Got error during run",e);
+		}
+	}
 }
